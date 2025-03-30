262a32ba87e274f4a6a8fe983cc5ef06e593885c
BBeerBear <xiongx659@gmail.com>
2025-03-30 17:44:06 -0400

    cookie-based authentication in JWT

--- src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
+++ src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
@@ -1,14 +1,17 @@
 package com.ecommerce.project.security.jwt;
 
+import com.ecommerce.project.security.services.UserDetailsImpl;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.WebUtils;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
@@ -18,29 +21,50 @@ import java.util.Date;
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-    @Value("${spring.app.jwtSecret}")
+    @Value("${spring.ecom.app.jwtSecret}")
     private String jwtSecret;
 
-    @Value("${spring.app.jwtExpirationMs}")
+    @Value("${spring.ecom.app.jwtExpirationMs}")
     private int jwtExpirationMs;
 
-    public String getJwtFromHeader(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        logger.debug("Authorization Header: {}", bearerToken);
-        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7); // Remove Bearer prefix
+    @Value("${spring.ecom.app.jwtCookieName}")
+    private String jwtCookie;
+
+//    public String getJwtFromHeader(HttpServletRequest request) {
+//        String bearerToken = request.getHeader("Authorization");
+//        logger.debug("Authorization Header: {}", bearerToken);
+//        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+//            return bearerToken.substring(7); // Remove Bearer prefix
+//        }
+//        return null;
+//    }
+
+    public String getJwtFromCookies(HttpServletRequest request) {
+        Cookie cookie = WebUtils.getCookie(request, jwtCookie);
+        if (cookie != null) {
+            return cookie.getValue();
+        } else {
+            return null;
         }
-        return null;
     }
 
-    public String generateTokenFromUsername(UserDetails userDetails) {
-        String username = userDetails.getUsername();
+
+    public String generateTokenFromUsername(String username) {
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date())
                 .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(key())
                 .compact();
+    }
+
+    public ResponseCookie generateJwtCookie(UserDetailsImpl userPrincipal) {
+        String jwt = generateTokenFromUsername(userPrincipal.getUsername());
+        return ResponseCookie.from(jwtCookie, jwt)
+                .path(("/api"))
+                .maxAge(24 * 60 * 60)
+                .httpOnly(false)
+                .build();
     }
 
     public String getUserNameFromJwtToken(String token) {
--- src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
+++ src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
@@ -7,15 +7,14 @@ import java.util.List;
 @Data
 public class UserInfoResponse {
     private Long id;
-    private String jwtToken;
+//    private String jwtToken; transform from header based to cookie based
     private String username;
     private List<String> roles;
 
-    public UserInfoResponse(Long id, String username, List<String> roles, String jwtToken) {
+    public UserInfoResponse(Long id, String username, List<String> roles) {
         this.id = id;
         this.username = username;
         this.roles = roles;
-        this.jwtToken = jwtToken;
     }
 }
 
